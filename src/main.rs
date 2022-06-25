284ae4b3b3ce9894458e16953bdeca1d1d258d3c
Arne Eckel <arneeckel@gmail.com>
2022-06-25 13:08:25 +0200

    added a method for showing map added the position of figure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct PositionFigure{
+    front: String,
+    right: String,
+    left: String,
+    back: String
+}
+
+impl PositionFigure{
+    fn create (front: String, right: String, left: String, back: String)->PositionFigure{
+        PositionFigure{front,right,left, back }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let pos:PositionFigure = PositionFigure::create(String::from("Wald"), String::from("Steppe"), String::from("Mauer"), String::from("Mauer"));
+    show_map(pos);
+}
+
+
+fn show_map(pos: PositionFigure){
+    println!("Vor dir befindet sich {}", pos.front);
+    println!("Rechts von dir befindet sich {}", pos.right);
+    println!("Links von dir befindet sich {}", pos.left);
+    println!("Hinter dir befindet sich {}", pos.back);
 }
