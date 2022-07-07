c041c43b3581cd67c6f54c226996099938f5e3dc
Arne Eckel <arneeckel@gmail.com>
2022-07-07 20:50:43 +0200

    add function-stub.

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,17 @@ impl PositionFigure{
 
 
 fn main() {
+    load_map("maps/first_map.map");
     let pos:PositionFigure = PositionFigure::create(String::from("Wald"), String::from("Steppe"), String::from("Mauer"), String::from("Mauer"));
     show_map(pos);
 }
 
+fn load_map(map_file: &str){
+    println!("{map_file}");
+    // TODO the function should load the map file and store it in a variable.
+}
 
+// TODO show_map should be a function of a map-variable
 fn show_map(pos: PositionFigure){
     println!("Vor dir befindet sich {}", pos.front);
     println!("Rechts von dir befindet sich {}", pos.right);
